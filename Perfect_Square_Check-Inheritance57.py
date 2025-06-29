5b1681cacb9142e504c08efc1405e65a5f92c8d1
MUTHAMIL S <s.muthamil05@gmail.com>
2025-06-29 18:32:36 +0530

    Create Perfect_Square_Check-Inheritance57.py

---
+++ Perfect_Square_Check-Inheritance57.py
@@ -0,0 +1,35 @@
+/*You are tasked with designing a class hierarchy to perform calculations on numbers. The first task is to calculate the sum of the digits of a number. Then, we need to check if the sum of the digits is a perfect square.
+
+Create a base class named NumberCalc with the following characteristics:
+An attribute number that stores an integer.
+A method inputNumber() that takes user input for the number attribute.
+A method calculateSumOfDigits() that calculates and returns the sum of the digits of the entered number.
+Implement a child class named PerfectSquareCheck that inherits from the NumberCalc class. The PerfectSquareCheck class should have the following characteristics:
+A method isPerfectSquare() that checks whether the sum of the digits is a perfect square and returns a boolean value.
+
+Input Format:
+A single integer representing the number.
+
+Output Format:
+The sum of the digits of the entered number.
+A boolean value(True/False) indicating whether the sum is a perfect square, each printed on a new line.
+Hint: To check if a number is a perfect square, you can use the math module's sqrt() function. If the square of the square root equals the number, then it is a perfect square.
+*/
+
+import math
+class NumberCalc:
+  def __init__(self):
+    self.number=0
+  def inputNumber(self):
+    self.number=int(input())
+  def calculateSumOfDigits(self):
+    return sum(int(i) for i in str(self.number))
+class PerfectSquareCheck(NumberCalc):
+  def isPerfectSquare(self):
+    s_digits=self.self.calculateSumOfDigits()
+    sv=int(math.sqrt(s_digits))
+    return sv*sv==s_digits
+calc=PerfectSquareCheck()
+calc.inputNumber()
+print(calc.calculateSumOfDigits())
+print(calc.isPerfectSquare())
